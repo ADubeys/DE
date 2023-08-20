3fb9756d0f75e52c894fbb45290d162171bc9359
ADubeys <ADubeys>
2023-08-20 07:02:11 +0000

    commit my test notebook

---
+++ Notebook Basics (1).py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print("hello World")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC Select "Hello"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC # My Test
+
+# COMMAND ----------
+
+
